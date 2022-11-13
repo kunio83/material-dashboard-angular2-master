52a67826fbdfa19c51cc4bc7d8a3c7e783c12776
Facundo <ezequiel-mu@live.com.ar>
2022-11-13 20:52:49 -0300

    image on items

--- src/app/businessConfig/businessConfig.component.ts
+++ src/app/businessConfig/businessConfig.component.ts
@@ -13,10 +13,16 @@ import { environment } from 'environments/environment';
 export class BusinessConfigComponent implements OnInit { 
     itemForms: UntypedFormArray = this.fb.array([]);
     notification = null;
+
+    imageError: string;
+    isImageSaved: boolean;
+    cardImageBase64: string;
     
     constructor(
         private fb: UntypedFormBuilder,
         private businessConfigService: BusinessConfigService) { }
+
+
     ngOnInit(): void {
         this.businessConfigService.getBusinessConfig(environment.tenantId).subscribe(
             res => {
@@ -77,5 +83,66 @@ export class BusinessConfigComponent implements OnInit {
         setTimeout(() => {
             this.notification = null;
         }, 3000);
+    }
+
+
+    fileChangeEvent(fileInput: any) {
+        this.imageError = null;
+        if (fileInput.target.files && fileInput.target.files[0]) {
+            // Size Filter Bytes
+            const max_size = 200000;
+            const allowed_types = ['image/png', 'image/jpeg'];
+            const max_height = 15200;
+            const max_width = 25600;
+
+            if (fileInput.target.files[0].size > max_size) {
+                this.imageError =
+                    'Maximum size allowed is ' + max_size / 1000 + 'Kb';
+
+                return false;
+            }
+
+            /*
+            if (!_.includes(allowed_types, fileInput.target.files[0].type)) {
+                this.imageError = 'Only Images are allowed ( JPG | PNG )';
+                return false;
+            }*/
+
+            const reader = new FileReader();
+            reader.onload = (e: any) => {
+                const image = new Image();
+                image.src = e.target.result;
+                image.onload = rs => {
+                    const img_height = rs.currentTarget['height'];
+                    const img_width = rs.currentTarget['width'];
+
+                    console.log(img_height, img_width);
+
+
+                    if (img_height > max_height && img_width > max_width) {
+                        this.imageError =
+                            'Maximum dimentions allowed ' +
+                            max_height +
+                            '*' +
+                            max_width +
+                            'px';
+                        return false;
+                    } else {
+                        const imgBase64Path = e.target.result;
+                        this.cardImageBase64 = imgBase64Path;
+                        this.isImageSaved = true;
+                        // this.previewImagePath = imgBase64Path;
+                        console.log(this.cardImageBase64);
+                    }
+                };
+            };
+
+            reader.readAsDataURL(fileInput.target.files[0]);
+        }
+    }
+
+    removeImage() {
+        this.cardImageBase64 = null;
+        this.isImageSaved = false;
     }
 }
